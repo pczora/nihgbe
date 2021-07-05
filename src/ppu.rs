7cae879fd2dc97461eaef95c58e0b4a082bc5568
Philipp Czora <philipp@czora.dev>
2021-07-05 22:39:40 +0200

    Adds LCD status updates

--- src/ppu.rs
+++ src/ppu.rs
@@ -6,13 +6,14 @@ pub struct PPU {
 }
 
 const ADDR_LY: u16 = 0xff44;
+const ADDR_LSTAT: u16 = 0xff41;
 
 impl PPU {
     pub fn update(&self, cycles: u8, mem: &mut mem::Mem) -> PPU {
         if self.display_disabled(mem) {
             return *self;
         }
-
+        self.update_status(mem);
         if self.scanline_counter <= 0 {
             mem.write(ADDR_LY, mem.read(ADDR_LY) + 1);
             let current_line = mem.read(ADDR_LY);
@@ -39,6 +40,35 @@ impl PPU {
             true
         };
     }
+
+    fn update_status(&self, mem: &mut mem::Mem) {
+        // TODO: Set coincidence flag
+        match self.scanline_counter {
+            0..=80 => {
+                mem.set_bit(ADDR_LSTAT, 1);
+                mem.reset_bit(ADDR_LSTAT, 0);
+                //TODO: Interrupt
+            }
+            81..=248 => {
+                mem.set_bit(ADDR_LSTAT, 1);
+                mem.set_bit(ADDR_LSTAT, 0);
+                //TODO: Interrupt
+
+            }
+            249..=456 => {
+                mem.reset_bit(ADDR_LSTAT, 1);
+                mem.reset_bit(ADDR_LSTAT, 0);
+                //TODO: Interrupt
+
+            }
+            _ => {
+                mem.reset_bit(ADDR_LSTAT, 1);
+                mem.set_bit(ADDR_LSTAT, 0);
+                //TODO: Interrupt
+            }
+        }
+    }
+
 }
 
 pub fn init_ppu() -> PPU {
