d30674150adf8a220b234743221a176f1a91aacb
Philipp Czora <philipp@czora.dev>
2021-09-27 21:26:09 +0200

    Set game name as window title

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 extern crate sdl2;
 
-use crate::cpu::CPU;
-use crate::mem::Mem;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
@@ -10,9 +8,9 @@ use std::fs;
 use std::time::Duration;
 
 mod cpu;
+mod debug;
 mod mem;
 mod ppu;
-mod debug;
 mod registers;
 
 const TITLE_START: u16 = 0x0134;
@@ -28,10 +26,11 @@ fn main() {
     let mut ppu = ppu::init_ppu();
     let mut mem = mem::init_mem(boot_rom, cart);
 
+    let title = parse_title(&mem);
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
-        .window("nihgbe", 160, 144)
+        .window(&title, 160, 144)
         .position_centered()
         .build()
         .unwrap();
@@ -69,6 +68,8 @@ fn main() {
 
 fn parse_title(mem: &mem::Mem) -> String {
     let title_vec = mem.read_range(TITLE_START..TITLE_END);
-    let title_string = String::from_utf8(title_vec);
-    return title_string.expect("Could not parse title");
+    let title_string = String::from_utf8(title_vec).expect("Could not parse title");
+
+    // Game titles are padded with NUL bytes; we need to remove them
+    return String::from(title_string.trim_end_matches(char::from('\0')));
 }
